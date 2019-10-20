b817a52e19bde5ae5c617bddf796541352a62214
Eric Manning <emanningmusic@yahoo.com>
2019-10-20 22:39:24 +0000

    Automatically backed up by Learn

--- lib/practicing_returns.rb
+++ lib/practicing_returns.rb
@@ -4,9 +4,9 @@ require 'pry'
  # i = 0
   #while i < array.length
    # yield(array[i])
-    i += 1
-  end
-end
+   # i += 1
+ # end
+#end
 
 #binding.pry
 #hello(["Tim", "Tom", "Jim"]) { |name| puts "Hi, #{name}" }
