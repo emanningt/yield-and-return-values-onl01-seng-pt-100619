344f2a9fb07eaeba72886a3698120ab1aa6b7f34
Eric Manning <emanningmusic@yahoo.com>
2019-10-20 22:39:15 +0000

    Automatically backed up by Learn

--- lib/practicing_returns.rb
+++ lib/practicing_returns.rb
@@ -1,12 +1,26 @@
 require 'pry'
 
-def hello(array)
-  i = 0
-  while i < array.length
-    yield(array[i])
+#def hello(array)
+ # i = 0
+  #while i < array.length
+   # yield(array[i])
     i += 1
   end
 end
 
-binding.pry
+#binding.pry
+#hello(["Tim", "Tom", "Jim"]) { |name| puts "Hi, #{name}" }
+
+def my_collect(array)
+  i = 0 
+  collection = []
+  while i < 
+  array.length
+  collection <<
+  yield(array[i])
+  i += 1 
+end 
+array
+end
+
 hello(["Tim", "Tom", "Jim"]) { |name| puts "Hi, #{name}" }
